241322493bb039c0ff043d1a29c761dd34a45a32
Jaskeir <rojasezequielsebastian@hotmail.com>
2025-05-09 00:17:05 -0300

    Pagina Datos Cliente
    Agregué la pagina para la carga de los datos del clientes luego de que se valida el código del voucher. Como les comentaba, no pude hacer andar la opción de MASTER PAGE, ni pagina con marter page, por mas que desinstale  eintaele como 5 veces los componentes asique lo hice un una nueva "pagina común".

---
+++ TP Promo Web/Datos Cliente.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TP_Promo_Web
+{
+    public partial class Datos_Cliente : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCanjear_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
