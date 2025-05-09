be4be45970f9e48c0f38d0345034770f0ba216cd
Salchy <leandrocg524@gmail.com>
2025-05-09 17:23:32 -0300

    Añadido de busqueda de artículos a la DB

--- TP Promo Web/dominio/Articulo.cs
+++ TP Promo Web/dominio/Articulo.cs
@@ -13,7 +13,7 @@ namespace dominio
         public string Codigo { get; set; }
         public string Nombre { get; set; }
         public string Descripcion { get; set; }
-        public Marca Marca {  get; set; }
+        public Marca Marca { get; set; }
         public Categoria Categoria { get; set; }
         public List<Imagen> Imagenes { get; set; }
         public decimal Precio { get; set; }
